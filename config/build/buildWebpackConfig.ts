5c19f066abc537ecf1394f02c2f39343e49c75f6
deniso4ka <den.zagrosh@gmail.com>
2022-09-23 23:53:15 +0300

    feat: initial commit

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,34 @@
+import path from 'path'
+import webpack from 'webpack'
+
+
+import { BuildOptions } from './types/config'
+
+
+import { buildPlugins } from './buildPlugins'
+import { buildLoaders } from './buildLoader'
+import { buildResolvers } from './buildResolvers'
+import { buildDevServer } from './buildDevServer'
+
+
+export function buildWebpackConfiguration(options: BuildOptions) {
+
+   const { paths, mode, isDev } = options
+
+   return {
+      mode,
+      entry: paths.entry,
+      output: {
+         filename: '[name].js',
+         path: paths.build,
+         clean: true
+      },
+      plugins: buildPlugins(options),
+      module: {
+         rules: buildLoaders(options),
+      },
+      resolve: buildResolvers(),
+      devtool: isDev ? 'inline-source-map' : undefined,
+      devServer: isDev ? buildDevServer(options) : undefined
+   }
+}
